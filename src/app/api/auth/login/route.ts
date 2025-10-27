f8355ad5cd87100480532a370ec0c06a9e614ccc
jihaneol <6531z@naver.com>
2025-10-27 21:06:20 +0900

    로그인 관련 기능 추가 - login - logout - user 정보

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,26 @@
+// /app/api/auth/login/route.ts
+import { NextRequest, NextResponse } from 'next/server';
+import { getIronSession } from 'iron-session';
+import { sessionOptions, SessionData } from '@/lib/sessionOptions';
+
+export async function POST(req: NextRequest) {
+    const { user_name, password } = await req.json();
+    const ADMIN_PASSWORD = process.env.NEXT_PUBLIC_ADMIN_PASSWORD;
+
+    if (!user_name || !password) {
+        return NextResponse.json({ error: 'Bad Request' }, { status: 400 });
+    }
+    const res = NextResponse.json({ ok: true });
+    const session = await getIronSession<SessionData>(req, res, sessionOptions);
+
+    console.log(password === ADMIN_PASSWORD )
+
+    if (password === ADMIN_PASSWORD) {
+        session.user = { name: user_name, role: 'admin' };
+    } else {
+        return NextResponse.json({ error: 'fail password' }, { status: 403 });
+    }
+
+    await session.save(); // 여기서 쿠키에 암호화/서명되어 저장됨
+    return res;
+}
---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,11 @@
+// /app/api/auth/logout/route.ts
+import { NextRequest, NextResponse } from 'next/server';
+import { getIronSession } from 'iron-session';
+import { sessionOptions, SessionData } from '@/lib/sessionOptions';
+
+export async function POST(req: NextRequest) {
+    const res = NextResponse.json({ ok: true });
+    const session = await getIronSession<SessionData>(req, res, sessionOptions);
+    await session.destroy(); // 쿠키 제거
+    return res;
+}
