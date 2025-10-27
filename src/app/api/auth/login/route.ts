d2d2dfe6be79c0480cad46b76f441066bc7978c2
jihaneol <6531z@naver.com>
2025-10-28 01:54:37 +0900

    authenticated 추가

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,4 +1,3 @@
-// /app/api/auth/login/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { getIronSession } from 'iron-session';
 import { sessionOptions, SessionData } from '@/lib/sessionOptions';
@@ -13,14 +12,11 @@ export async function POST(req: NextRequest) {
     const res = NextResponse.json({ ok: true });
     const session = await getIronSession<SessionData>(req, res, sessionOptions);
 
-    console.log(password === ADMIN_PASSWORD )
-
     if (password === ADMIN_PASSWORD) {
-        session.user = { name: user_name, role: 'admin' };
+        session.user = {authenticated:true, name: user_name, role: 'admin' };
     } else {
-        return NextResponse.json({ error: 'fail password' }, { status: 403 });
+        return NextResponse.json({ error: 'fail password' }, { status: 401 });
     }
-
     await session.save(); // 여기서 쿠키에 암호화/서명되어 저장됨
     return res;
 }
